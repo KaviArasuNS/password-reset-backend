f7f356c66aff4f0454ae4930708971eff215a9ef
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-09-12 18:10:36 +0530

    comit

--- models/user.js
+++ models/user.js
@@ -26,7 +26,6 @@ const validate = (data) => {
     lastName: Joi.string().required().label("Last Name"),
     email: Joi.string().email().required().label("Email"),
     password: passwordComplexity().required().label("Password"),
-    verified: { type: Boolean, default: false },
   });
   return schema.validate(data);
 };
